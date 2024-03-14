5ca06dcf75b2dd887b4fdcce84847daca87b4b4d
Ivan Pronin <pacernestit@rambler.ru>
2024-03-14 23:10:54 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+function App() {
+  return (
+    <>
+      <Login />
+      <Register />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,75 @@
+import "../styles/Login.css";
+import registerImage from "../assets/images/registerImage.jpg";
+import { TextField, Checkbox, Button } from "@mui/material";
+
+export default function Login() {
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <div className="login-field">
+            <header className="header">
+              <div className="header-img">
+                <img src={registerImage} alt="registerimg" />
+              </div>
+              <div className="header-title">
+                <span>Вход в Allah.cc</span>
+                <p>
+                  Ещё нет аккаунта? <a href="#">Регистрация</a>
+                </p>
+              </div>
+
+              <div className="login-valid-info"></div>
+
+              <form
+                action="http://localhost:4200/"
+                method="post"
+                className="input-field"
+              >
+                <TextField
+                  size="small"
+                  label="Логин"
+                  placeholder="Логин"
+                  variant="outlined"
+                  type="text"
+                  className="text-field"
+                />
+
+                <TextField
+                  size="small"
+                  label="Пароль"
+                  placeholder="Пароль"
+                  variant="outlined"
+                  type="password"
+                  className="text-field"
+                />
+
+                <div className="input-accept-rules">
+                  <Checkbox size="small" />
+                  <p className="checkbox-text">Оставаться в системе</p>
+                  <a>Забыли пароль?</a>
+                </div>
+
+                <Button
+                  sx={{
+                    background: "#7c2d12",
+                    height: "44px",
+                    width: "320px",
+                    borderRadius: "0.375rem",
+                    color: "rgba(203, 213, 225, 1)",
+                    textTransform: "none",
+                    border: "1px solid #E6563D",
+                    "&:hover": { background: "#E6563D" },
+                  }}
+                  variant="contained"
+                >
+                  Войти
+                </Button>
+              </form>
+            </header>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
